73ec82a9b9d6c3885a64ac743bfd44ed0d34982f
b-s-x <b.s.x.mars@gmail.com>
2022-02-13 21:04:42 +0300

    small fix

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -1,6 +1,10 @@
-<script>
+<script >
 import { defineComponent } from 'vue';
 
-const Bar = defineComponent({ render() { return <div>Test</div> }})
+const Bar = defineComponent({
+  render() {
+    return <div>Test</div>
+  }})
+
 export { Bar }
 </script>
