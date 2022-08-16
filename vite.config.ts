54c1b670b443d7ff036dd9e6dc9065004a293d21
b-s-x <b.s.x.mars@gmail.com>
2022-08-16 20:19:38 +0300

    vite.config.ts: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve:{
+    alias:{
+      '@' : path.resolve(__dirname, './src'),
+      '@common': path.resolve(__dirname, './src/styles/common.scss')
+    },
+  },
   plugins: [
     vue(),
     vueJsx(),
